1ce89d0ce343ea33ab87b63240c78ab25d864060
Nick Dichev <github@dichev.email>
2024-10-07 18:32:45 -0700

    Add index to brand on brand_reviews

---
+++ backend/migrations/1728351125_updated_brand_reviews.go
@@ -0,0 +1,41 @@
+package migrations
+
+import (
+	"encoding/json"
+
+	"github.com/pocketbase/dbx"
+	"github.com/pocketbase/pocketbase/daos"
+	m "github.com/pocketbase/pocketbase/migrations"
+)
+
+func init() {
+	m.Register(func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("6aseqnkss24vyg4")
+		if err != nil {
+			return err
+		}
+
+		if err := json.Unmarshal([]byte(`[
+			"CREATE INDEX ` + "`" + `idx_BoXbxGR` + "`" + ` ON ` + "`" + `brand_reviews` + "`" + ` (` + "`" + `brand` + "`" + `)"
+		]`), &collection.Indexes); err != nil {
+			return err
+		}
+
+		return dao.SaveCollection(collection)
+	}, func(db dbx.Builder) error {
+		dao := daos.New(db);
+
+		collection, err := dao.FindCollectionByNameOrId("6aseqnkss24vyg4")
+		if err != nil {
+			return err
+		}
+
+		if err := json.Unmarshal([]byte(`[]`), &collection.Indexes); err != nil {
+			return err
+		}
+
+		return dao.SaveCollection(collection)
+	})
+}
