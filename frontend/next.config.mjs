2a341cfdb9294e2561169055029484d7fa127935
Nick Dichev <github@dichev.email>
2024-10-20 12:35:24 -0700

    Add temporary domain for serving images

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['127.0.0.1', 'portalwholesale.com'],
+    domains: ['127.0.0.1', 'portalwholesale.com', 'zs40048k4ocgcg8c4ccowo0k.dichev.io'],
   },
 };
 
